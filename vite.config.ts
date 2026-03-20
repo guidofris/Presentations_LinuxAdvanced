d00af164d8760f51c94ae16878ed2d59aa079b28
Guido Frissaer <guido.frissaer@pxl.be>
2026-03-20 15:09:17 +0100

    fixing it for safari as well

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Presentations_LinuxAdvanced/',
+  base: './',
   plugins: [react()],
 })
